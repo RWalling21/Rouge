64dd7d90a5c7e7fa32aa7b102710c63ce313276a
RWalling21 <rhw8246@rit.edu>
2024-05-27 16:29:16 -0400

    Built simple HTTP proxy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use tower::make::Shared;
+
+use hyper::{service::service_fn, Body, Client, Request, Response, Server};
+
+async fn handle(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let client = Client::new();
+    client.request(req).await
 }
+
+#[tokio::main]
+async fn main() {
+    let make_service = Shared::new(service_fn(handle));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let server = Server::bind(&addr).serve(make_service);
+
+    if let Err(e) = server.await {
+        println!("Error: {}", e);
+    }
+} 
